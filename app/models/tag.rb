031468fa4fea1d604c9b63b7d168561e5643a5b8
YuriiHorbach <gyrii84@gmail.com>
2020-08-29 16:09:13 +0300

    add tag cloud to blog

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
-	has_many :tagging
-	has_many :posts, through: :tagging
+	has_many :taggings
+	has_many :posts, through: :taggings
+
+	def self.counts
+		self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+	end
+
 end
